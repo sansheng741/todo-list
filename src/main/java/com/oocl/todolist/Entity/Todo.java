b4b6eb45549b3db3d1a9660fa8cb55b7cb2305d7
sansheng741 <807156237@qq.com>
2020-08-06 19:59:47 +0800

    [todolist] [colin & alice] feature : create entity todo

--- src/main/java/com/oocl/todolist/Entity/Todo.java
+++ src/main/java/com/oocl/todolist/Entity/Todo.java
@@ -1,6 +1,10 @@
 package com.oocl.todolist.Entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by CHENCO9 on 8/6/2020 7:51 PM
@@ -8,5 +12,40 @@ import javax.persistence.Entity;
  */
 @Entity
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @NotNull
+    private String content;
+    @NotNull
+    private boolean status;
+
+
+    public Todo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
