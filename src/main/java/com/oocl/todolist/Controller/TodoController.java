3254f0e3762538bc8a9e42d38e35f1df4d048e99
sansheng741 <807156237@qq.com>
2020-08-06 21:53:30 +0800

    [todolist] [colin & alice] feature: add Controller method queryAll

--- src/main/java/com/oocl/todolist/Controller/TodoController.java
+++ src/main/java/com/oocl/todolist/Controller/TodoController.java
@@ -1,12 +1,30 @@
 package com.oocl.todolist.Controller;
 
+import com.oocl.todolist.Dto.TodoResponse;
+import com.oocl.todolist.Service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by CHENCO9 on 8/6/2020 8:03 PM
  */
 @RestController
+@RequestMapping("/todos")
 public class TodoController {
+
+    @Autowired
+    TodoService todoService;
+
+    @GetMapping
+    public List<TodoResponse> queryAll(){
+        return todoService.queryAll();
+    }
+
 
 
 
